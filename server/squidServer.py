11f72b9c9045ec39785cc15bcb4e3d4802d49146
Jason Walker <jasonalwalker@gmail.com>
2015-09-16 15:03:12 -0600

    Update squidServer.py

--- server/squidServer.py
+++ server/squidServer.py
@@ -1,7 +1,7 @@
 #Team Fork Bomb
 
-#Casey Freeburg, Jason Walker, Levi Muniz, Morgan Brown, Nicholas Coiner
-
+v = Casey Freeburg, Jason Walker, Levi Muniz, Morgan Brown, Nicholas Coiner
+print(v)
 
 '''
 Something VERY experimental
